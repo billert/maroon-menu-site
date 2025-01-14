d31972872ee336db28a8737e6dc918e7f6d5b4f5
Owen Cai <owenbcai@gmail.com>
2025-01-14 16:00:15 -0600

    added final page! yipee

--- app/page.tsx
+++ app/page.tsx
@@ -79,8 +79,8 @@ export default function Home() {
         </motion.div>
         <motion.p style = {{originX: 0.5, originY: 0.5}}  initial={{ opacity: 0.2 }} whileInView={{ opacity: 0.8 }} transition={{ type: "spring", bounce: 0.1, duration: 0.25, delay: 1.5}} viewport={{ once: true }} className="text-md md:text-2xl text-center">Maroon Menu makes that choice simpler.</motion.p>
         <div className = "mt-10 justify-center items-center flex flex-col">
-          <motion.a href="#question" style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} animate={{ opacity: 0.6 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 3.5}} className="text-2xl">Scroll</motion.a>
-          <motion.a style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 3.5}} href = "#theresmore">
+          <motion.a href="#theresmore" style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} whileInView={{ opacity: 0.6 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 2}} className="text-2xl">Scroll</motion.a>
+          <motion.a style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 2}} href = "#theresmore">
             <motion.div initial = {{y: 0}} animate = {{y: 10}} transition = {{repeat: Infinity, repeatType: "reverse", duration: 1}}>
               <Image src = "/arrow-right.svg" alt="Down arrow" width={50} height={50} className = "rotate-90 dark:invert opacity-60"></Image>
             </motion.div>
@@ -89,7 +89,7 @@ export default function Home() {
       </div>
       <div id="theresmore" className = "min-h-screen justify-center items-center flex flex-col pt-40">
         
-        <motion.p style = {{originX: 0.5, originY: 0.5}}  initial={{ opacity: 0.2 }} whileInView={{ opacity: 0.8 }} transition={{ type: "spring", bounce: 0.1, duration: 0.5, delay: 0.5}} viewport={{ once: true }} className="text-md md:text-2xl text-center">But wait! There&apos;s more:</motion.p>
+        <motion.p style = {{originX: 0.5, originY: 0.5}}  initial={{ opacity: 0.2 }} whileInView={{ opacity: 0.8 }} transition={{ type: "spring", bounce: 0.1, duration: 0.5, delay: 0.0}} viewport={{ once: true }} className="text-md md:text-2xl text-center">But wait! There&apos;s more:</motion.p>
         <Accordion variant = "bordered" itemClasses = {{title: "text-4xl"}}>
           <AccordionItem key="1" aria-label="Accordion 1" title={<motion.p style = {{originX: 0.5, originY: 0.5}}  initial={{ scale: 0 }} whileInView={{ scale: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 0.1 }} className= "text-5xl font-extrabold bg-gradient-to-r from-[#ffd359] to-[#ffb700] inline-block text-transparent bg-clip-text text-center pb-1">⭐ Star your favorite foods</motion.p>}>
             <p className = "max-w-2xl">Was there ever a time you wanted to let your future self know that a certain meal was good? With stars, now you can!</p>
@@ -107,10 +107,28 @@ export default function Home() {
             <p className = "max-w-2xl">With haptic feedback, buttons and pickers feel more clicky and satisfying. It&apos;s almost like you&apos;re pressing a real button.</p>
           </AccordionItem>
         </Accordion>
+        <div className = "mt-10 justify-center items-center flex flex-col">
+          <motion.a href="#tryit" style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} whileInView={{ opacity: 0.6 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 1.5}} className="text-2xl">Scroll</motion.a>
+          <motion.a style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 1.5}} href = "#tryit">
+            <motion.div initial = {{y: 0}} animate = {{y: 10}} transition = {{repeat: Infinity, repeatType: "reverse", duration: 1}}>
+              <Image src = "/arrow-right.svg" alt="Down arrow" width={50} height={50} className = "rotate-90 dark:invert opacity-60"></Image>
+            </motion.div>
+          </motion.a>
+        </div>
         {/*<motion.p style = {{originX: 0.5, originY: 0.5}}  initial={{ opacity: 0.2 }} whileInView={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 2.5, delay: 2}} viewport={{ once: true }} className="text-4xl md:text-6xl font-extrabold bg-gradient-to-t to-[#0980ff] from-[#59a5fc] text-white text-center p-1 rounded-xl">"Should I swipe in?"</motion.p>*/}
         
       </div>
-      <p className = "text-center text-3xl">yay the website is almost done!</p>
+      <div id="tryit" className = "min-h-screen justify-center items-center flex flex-col pt-40">
+        <motion.p style = {{originX: 0, originY: 0}}  initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 0.0}} className = "text-3xl md:text-5xl font-bold text-center">Give Maroon Menu a try</motion.p>
+        <motion.div className = "flex flex-row items-center justify-center gap-2 md:gap-5 mt-5 max-w-full p-2">
+            <motion.a initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 0.25 }} href = "https://apps.apple.com/us/app/maroon-menu/id6739734458">
+              <Image src = "/apple store.svg" alt="Apple App Store Download" width={200} height={100}></Image>
+            </motion.a>
+            <motion.a initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 0.5, delay: 0.25 }} href="https://play.google.com/store/apps/details?id=com.billert.maroonmenu&pcampaignid=web_share">
+              <Image src = "/google store.png" alt="Google Play Store Downlaod" width={220} height={100}></Image>
+            </motion.a>
+          </motion.div>
+      </div>
     </div>
   );
 }
