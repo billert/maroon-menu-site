b661bcfc982515195161467e2c5b55972a371823
Owen Cai <owenbcai@gmail.com>
2025-01-26 17:11:38 -0600

    support page fix apostrophes

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -2,8 +2,8 @@ export default function Page() {
     return (
         <div className = "p-3 pt-20 h-screen">
             <p className = "text-5xl font-bold mb-3">Frequently Asked Questions</p>
-            <h1 className = "text-lg font-bold">The menu isn't what's being served right now</h1>
-            <p className="opacity-75">To save you loading time, data is cached. Unfortunately, the menu sometimes changes during the day. Thankfully, the difference is usually minor, and we're working on addressing these cache misses. You can expect an fix by the end of February. </p>
+            <h1 className = "text-lg font-bold">The menu isn&apos;t what&apos;s being served right now</h1>
+            <p className="opacity-75">To save you loading time, data is cached. Unfortunately, the menu sometimes changes during the day. Thankfully, the difference is usually minor, and we&apos;re working on addressing these cache misses. You can expect an fix by the end of February. </p>
             <h1 className = "text-lg font-bold">Why does starring one item sometimes star another item?</h1>
             <p className="opacity-75">Stars are linked to a menu reference number in the database. This helps ensure that should a menu item&apos;s name be updated, it will remain starred. However, a small group of items share menu reference numbers with each other, which causes them to appear linked together.</p>
             <h1 className = "text-lg font-bold">Why can&apos;t I change the mealtime?</h1>
